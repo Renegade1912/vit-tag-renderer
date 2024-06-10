a3530617e1b9d551c44de17c5c1abba84be2594e
Renegade <renegade@unlimited.wtf>
2024-06-11 01:26:55 +0200

    add dynamic port

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import "dotenv/config";
+
 import Fastify from "fastify";
 import tagRoutes from "./modules/tag/tag.route";
 import { TagSchemas } from "./modules/tag/tag.schema";
 
 const server = Fastify();
+
+const port: number = parseInt(process.env.PORT ?? "3001");
 
 // Run the server!
 const main = async () => {
@@ -13,7 +17,7 @@ const main = async () => {
   server.register(tagRoutes, { prefix: "/api/tag" });
 
   try {
-    await server.listen({ port: 3001 });
+    await server.listen({ port });
   } catch (err) {
     server.log.error(err);
     process.exit(1);
